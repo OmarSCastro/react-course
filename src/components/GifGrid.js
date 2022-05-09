9e858c7dc2a90a2fbde5be0cd665c21a5b3afb73
OmarSCastro <99294488+OmarSCastro@users.noreply.github.com>
2022-05-09 17:33:40 -0500

    Updating project

---
+++ src/components/GifGrid.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useFetchGifs } from '../hooks/useFetchGifs'
+import { GifGridItems } from './GifGridItems';
+
+export const GifGrid = ( { category } ) => {
+
+    const {data:images , loading} = useFetchGifs ( category );
+
+  return (
+      
+    <>
+            <h3 className='animate__bounceIn'> { category } </h3>
+
+        {loading && <p className='animate__flash'>Loading...</p>   }
+
+        <div className='card-grid' >
+        
+                {
+                    images.map((img) => (
+                    <GifGridItems 
+                    key ={img.id} 
+                    {...img} />
+                    ))
+                }
+
+        </div>  
+    </>
+  )
+}
